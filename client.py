bc4718d391ecb79291b628f9014922f074143cd1
Apoorv <apoorvmishra05@gmail.com>
2020-04-01 05:42:22 +0530

    First Commit

---
+++ client.py
@@ -0,0 +1,46 @@
+import pygame
+from network import Network
+from player import Player
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+
+
+
+
+
+
+def redrawWindow(win, player, player2):
+
+	win.fill((255,255,255))
+	player.draw(win)
+	player2.draw(win)
+	pygame.display.update()
+
+
+
+
+def main():
+	n = Network()
+	p1 = n.getP()
+	run = True
+	clock = pygame.time.Clock()
+
+	while run:
+
+		clock.tick(60)
+		p2 = n.send(p1)
+		
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+				pygame.quit();
+
+		p1.move()
+		redrawWindow(win, p1, p2)
+
+main()
